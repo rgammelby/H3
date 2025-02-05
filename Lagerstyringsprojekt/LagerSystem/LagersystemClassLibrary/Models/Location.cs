5522646f57eea7c379483824334194a1ad549baa
Tian70777 <158053422+Tian70777@users.noreply.github.com>
2025-02-05 09:00:27 +0100

    Update Location.cs

--- Lagerstyringsprojekt/LagerSystem/LagersystemClassLibrary/Models/Location.cs
+++ Lagerstyringsprojekt/LagerSystem/LagersystemClassLibrary/Models/Location.cs
@@ -1,25 +1,27 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace LagerstyringClassLibrary.Models
-//{
-//    public class Location
-//    {
-//        public Location() { }
+namespace LagerstyringClassLibrary.Models
+{
+    public class Location
+    {
+        public Location() { }
 
-//        [Key]
-//        public int id { get; set; }
-//        public int designation { get; set; }  // FK LocationRoom  Room 1 Cupboard 1
-        
-//        // 1-m navigation prop
-//        ICollection<LocationRoom> Rooms { get; set; }
+        [Key]
+        public int id { get; set; }
+        public int room_id { get; set; }
+        public int cupboard_id { get; set; }
 
-//        // nav prop
-//        public SingleDevice SingleDevice { get; set; }
+        // 1-m navigation prop
+        public ICollection<LocationRoom> Rooms { get; set; }
+        public ICollection<LocationCupboard> Cupboards { get; set; }
 
-//    }
-//}
+        // nav prop
+        public ICollection<SingleDevice> Devices { get; set; } = new List<SingleDevice>();
+
+    }
+}
