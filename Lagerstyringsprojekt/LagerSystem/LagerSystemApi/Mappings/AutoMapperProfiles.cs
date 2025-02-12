5640e4b43977a49b80853a652deb5d6d3c081603
Tian70777 <158053422+Tian70777@users.noreply.github.com>
2025-02-12 09:31:39 +0100

    Update AutoMapperProfiles.cs

--- Lagerstyringsprojekt/LagerSystem/LagerSystemApi/Mappings/AutoMapperProfiles.cs
+++ Lagerstyringsprojekt/LagerSystem/LagerSystemApi/Mappings/AutoMapperProfiles.cs
@@ -7,7 +7,6 @@ namespace LagerSystemApi.Mappings
     {
         public AutoMapperProfiles() 
         {
-
             // Map between DeviceOverview and DeviceOverviewDTO
             // DeviceOverviewDTO: Used for retrieving data.
             CreateMap<DeviceOverview, DeviceOverviewDTO>().ReverseMap();
@@ -18,16 +17,20 @@ namespace LagerSystemApi.Mappings
             // UpdateDeviceOverviewDTO: Used for updating an existing device
             CreateMap<DeviceOverview, UpdateDeviceOverviewDTO>().ReverseMap();
 
-
+            / Map between Device and DeviceDTO
+            // DeviceDTO: Used for retrieving data.
             CreateMap<DeviceDTO, SingleDevice>().ReverseMap();
 
-            //CreateMap<AddSingleDeviceDTO, SingleDevice>().ReverseMap();
             //Mapping for Adding a New Device (DTO → Domain)
             CreateMap<AddSingleDeviceDTO, SingleDevice>()
                 .ForMember(dest => dest.Location, opt => opt.Ignore());  //  Ignore Navigation Property
-
+            
+            // UpdateDeviceDTO: Used for updating an existing device
             CreateMap<UpdateDeviceDTO, SingleDevice>()
                 .ForMember(dest => dest.Location, opt => opt.Ignore());  //  Ignore Navigation Property
+            
+            // only mapping from Log (domain model) to LogDTO (Data Transfer Object).
+            CreateMap<Log, LogDTO>();
         }
     }
 }
