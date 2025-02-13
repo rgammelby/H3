b551b388ef3b9883683718bb9f7256302d875790
Tian70777 <158053422+Tian70777@users.noreply.github.com>
2025-02-13 16:36:42 +0100

    Create LocationCupboardDTO.cs

---
+++ Lagerstyringsprojekt/LagerSystem/LagerSystemApi/Models/DTO/LocationCupboardDTO.cs
@@ -0,0 +1,9 @@
+namespace LagerSystemApi.Models.DTO
+{
+    public class LocationCupboardDTO
+    {
+        public int id { get; set; }
+        public string designation { get; set; }
+        public int room_id { get; set; }
+    }
+}
